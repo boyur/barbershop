5ad0b75cf503b15e503bc17a86e162fc85fcb141
Artem Boyur <gboyur@gmail.com>
2016-11-21 00:07:37 +0300

    gulp: add copy files and sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const csso = require('gulp-csso');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
+const clean = require('gulp-dest-clean');
 const browserSync = require('browser-sync').create();
 
 let scssLibs = [
@@ -42,11 +43,34 @@ gulp.task('pug', function buildHTML() {
     .pipe(gulp.dest('./public'));
 });
 
+// COPY
+
+gulp.task('copy:images', function () {
+    return gulp.src('./source/images/*')
+        .pipe(clean('./public/img'))
+        .pipe(gulp.dest('./public/img'))
+});
+
+gulp.task('copy:svg', function () {
+    return gulp.src('./source/svg/*')
+        .pipe(clean('./public/svg'))
+        .pipe(gulp.dest('./public/svg'))
+});
+
+gulp.task('copy:fonts', function () {
+    return gulp.src('./source/fonts/*')
+        .pipe(clean('./public/fonts'))
+        .pipe(gulp.dest('./public/fonts'))
+});
+
 // WATCH
 
 gulp.task('watch', function () {
   gulp.watch('./style/**/*.scss', gulp.series('scss'));
   gulp.watch('./views/**/*.pug', gulp.series('pug'));
+  gulp.watch('./source/images/*', gulp.series('copy:images'));
+  gulp.watch('./source/svg/*', gulp.series('copy:svg'));
+  gulp.watch('./source/fonts/*', gulp.series('copy:fonts'));
 });
 
 // SERVER
@@ -66,7 +90,13 @@ gulp.task('default', gulp.series(
   gulp.parallel(
     'scss',
     'css-libs',
-    'pug'
+    'pug',
+    'copy:images',
+    'copy:svg',
+    'copy:fonts'
   ),
-    'watch')
+  gulp.parallel(
+    'watch',
+    'browser-sync'
+  ))
 );
